aa0d8f3e076b20ddd589a8e151a3078e1be235c0
ThiagoDambroski <dambroskic@hotmail.com>
2022-11-03 07:25:12 -0300

    Configuring users InMemoryUserDetailsManager  1
    approach 1

--- src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
+++ src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.dambroski.SpringSecuirtyUdemy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +22,24 @@ public class ProjectSecurityConfig {
 		
 		
 		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("thiago")
+				.password("123")
+				.authorities("admin")
+				.build();
+	
+		UserDetails leticia = User.withDefaultPasswordEncoder()
+				.username("leticia")
+				.password("linda")
+				.authorities("read")
+				.build();
+	
+		return new InMemoryUserDetailsManager(admin,leticia);
 	}
 
 }
