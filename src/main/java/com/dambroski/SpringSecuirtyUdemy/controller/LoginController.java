5d5f928ebe607edf74d14704a27e31f457fe0cc2
ThiagoDambroski <dambroskic@hotmail.com>
2022-11-07 21:52:29 -0300

    local dataBase

--- src/main/java/com/dambroski/SpringSecuirtyUdemy/controller/LoginController.java
+++ src/main/java/com/dambroski/SpringSecuirtyUdemy/controller/LoginController.java
@@ -6,11 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dambroski.SpringSecuirtyUdemy.model.Customer;
 import com.dambroski.SpringSecuirtyUdemy.repository.CustomerRepository;
 
-@Service
+@RestController
 public class LoginController {
 	
 	@Autowired
