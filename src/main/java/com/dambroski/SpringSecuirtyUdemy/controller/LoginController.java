2cbf0df254fc766b084933fc5970d9b46bb53fc8
ThiagoDambroski <dambroskic@hotmail.com>
2022-11-08 09:37:34 -0300

    Demo of login with Bcrypt password encoder

--- src/main/java/com/dambroski/SpringSecuirtyUdemy/controller/LoginController.java
+++ src/main/java/com/dambroski/SpringSecuirtyUdemy/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.dambroski.SpringSecuirtyUdemy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,16 @@ public class LoginController {
 	@Autowired
 	CustomerRepository customerRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@PostMapping("/register")
 	public ResponseEntity<String> registerCustomer(@RequestBody Customer customer) {
 		Customer savedCustomer = null;
 		ResponseEntity response = null;
 		try {
+			String passwrod = passwordEncoder.encode(customer.getPwd());
+			customer.setPwd(passwrod);
 			savedCustomer = customerRepository.save(customer);
 			if(savedCustomer.getId() > 0) {
 				response = ResponseEntity.status(HttpStatus.CREATED).body("sucess");
