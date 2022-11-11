952085268f9747316d57c22bda3bb21d395b474d
ThiagoDambroski <dambroskic@hotmail.com>
2022-11-11 10:32:37 -0300

    Fixing CORs issue

--- src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
+++ src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.dambroski.SpringSecuirtyUdemy.config;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -11,15 +15,29 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 	
 	
+	
 	@Bean
 	SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception{
-		http
-		.csrf().disable()
+		http.cors().configurationSource(new CorsConfigurationSource() {
+			
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:4200"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}
+		}).and().csrf().disable()
 		.authorizeHttpRequests()
 			.antMatchers("/myAccount","myBalance","/myLoans","/myCards","/user").authenticated()
 			.antMatchers("/notices","/contact","/register").permitAll()
