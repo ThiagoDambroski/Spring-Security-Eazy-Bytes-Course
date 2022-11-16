8443f33d2b8b6ae6b3de627f565d12c65f003348
ThiagoDambroski <dambroskic@hotmail.com>
2022-11-16 12:37:55 -0300

    Custom filter before method

--- src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
+++ src/main/java/com/dambroski/SpringSecuirtyUdemy/config/ProjectSecurityConfig.java
@@ -1,50 +1,51 @@
 package com.dambroski.SpringSecuirtyUdemy.config;
 
-import java.util.Collection;
+
 import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.dambroski.SpringSecuirtyUdemy.filter.RequestValidationBeforeFilter;
 
 @Configuration
 public class ProjectSecurityConfig {
 	
 	
 	
-	@Bean
-	SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception{
-		http.cors().configurationSource(new CorsConfigurationSource() {
-			
-			@Override
-			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-				CorsConfiguration config = new CorsConfiguration();
-				config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:4200"));
-				config.setAllowedMethods(Collections.singletonList("*"));
-				config.setAllowCredentials(true);
-				config.setAllowedHeaders(Collections.singletonList("*"));
-				config.setMaxAge(3600L);
-				return config;
-			}
+	 @Bean
+	    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+	        http.securityContext().requireExplicitSave(false)
+	            .and().cors().configurationSource(new CorsConfigurationSource() {
+	            @Override
+	            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+	                CorsConfiguration config = new CorsConfiguration();
+	                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+	                config.setAllowedMethods(Collections.singletonList("*"));
+	                config.setAllowCredentials(true);
+	                config.setAllowedHeaders(Collections.singletonList("*"));
+	                config.setMaxAge(3600L);
+	                return config;
+	            }
+
+		
 		}).and().csrf().ignoringAntMatchers("/contact","/register")
 						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		.and().authorizeHttpRequests()
-			.antMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
-			.antMatchers("myBalance").hasAnyAuthority("VIEWACCOUNT","VIEWBALANCE")
-			.antMatchers("/myLoans").hasAuthority("VIEWLOANS")
-			.antMatchers("myCards").hasAuthority("VIEWCARDS")
+		.and().addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+		.authorizeHttpRequests()
+			.antMatchers("/myAccount").hasRole("USER")
+			.antMatchers("myBalance").hasAnyRole("USER","ADMIN")
+			.antMatchers("/myLoans").hasRole("USER")
+			.antMatchers("myCards").hasRole("USER")
 			.antMatchers("/user").authenticated()
 			.antMatchers("/notices","/contact","/register").permitAll()
 		.and().httpBasic()
